590b440b69c08afaee87fbd057697a6fdfd29935
mpanand1999 <mpanand1999@gmail.com>
2023-11-22 13:01:44 +0530

    read

---
+++ PYTHON/read ANAD write/test.py
@@ -0,0 +1,4 @@
+f1=open("abc.txt","w")
+print("name of the file;",f1.name)
+print("closed or not :", f1.closed)
+print("opening mode:",f1.mode)
---
+++ PYTHON/read ANAD write/test_2.py
@@ -0,0 +1,4 @@
+f=open("abc.txt","w")
+f.write("my 1st file")
+f.close()
+print("file",f.name,"closed")
